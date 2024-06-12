6c8a0fa6ef17fc7dd263948b24264fa4d7062142
thomasklz <pipos_rgt@hotmail.com>
2024-06-12 10:17:47 -0500

    conexion

--- base_de_datos/conexion.js
+++ base_de_datos/conexion.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE} from '../config/config.js';
+import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE,DB_PORT} from '../config/config.js';
 // Option 3: Passing parameters separately (other dialects)
 export const sequelize = new Sequelize(
     DB_DATABASE,
@@ -7,7 +7,8 @@ export const sequelize = new Sequelize(
     DB_PASSWORD,
     {
         host: DB_HOST,
-        dialect: DB_CONNECTION
+        dialect: DB_CONNECTION,
+        port: DB_PORT
     }
 );
 
